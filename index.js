33919213eb4d3ce044fc5b07afbc7e4fad2a4b71
Shreya Hegde <Shreya.Hegde@target.com>
2024-09-17 17:09:35 +0530

    github pages  commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { dockStart } = require("@nlpjs/basic");
-
-(async () => {
-  const dock = await dockStart();
-  const nlp = dock.get('nlp');
-  await nlp.train();
-})();
+document.addEventListener('DOMContentLoaded', async () => {
+    const { dockStart } = window.nlpjs;
+    const dock = await dockStart();
+    const nlp = dock.get('nlp');
+    await nlp.train();
+    document.getElementById('output').textContent = 'Training complete!';
+});
